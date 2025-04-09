e3c69ad7b12a023374929717029918381ee412b5
Steve Gibbs <swaygibb@gmail.com>
2025-04-09 14:56:27 -0500

    Update default date on model

--- Models/Task.cs
+++ Models/Task.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
         public bool IsCompleted { get; set; }
     }
 }
